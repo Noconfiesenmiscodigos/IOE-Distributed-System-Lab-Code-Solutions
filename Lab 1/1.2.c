4a89f5373f09c2cbf3cfdec1172a87bf70561dbd
133^4+134^4=59^4+158^4 <58982771+Noconfiesenmiscodigos@users.noreply.github.com>
2022-07-08 09:59:26 +0545

    Update 1.2.c

--- Lab 1/1.2.c
+++ Lab 1/1.2.c
@@ -7,21 +7,6 @@
 long *p1(int i,long *comp);
 long *p2(int i,long *comp);
 long *p3(int i,long *comp);
-void main()
-{
-    long start[]= {0,0,0},*vector;
-    while(!kbhit())
-    {
-        p1(1,&start[0]);
-    }
-    printf("\n Process Vector\n");
-    vector=p1(0,&start[0]);
-    printf("p1[%ld/t%ld/t%ld]\n",*vector,*(vector+1),*(vector+2));
-    vector=p2(0,&start[0]);
-    printf("p2[%ld/t%ld/t%ld]\n",*vector,*(vector+1),*(vector+2));
-    vector=p3(0,&start[0]);
-    printf("p3[%ld/t%ld/t%ld]\n",*vector,*(vector+1),*(vector+2));
-}
 long *p1(int i,long *comp)
 {
     static long a[]= {0,0,0};
@@ -31,7 +16,8 @@ long *p1(int i,long *comp)
         a[0]++;
         if(*(comp+1)>a[1]) a[1]=*(comp+1);
         if(*(comp+2)>a[2]) a[2]=*(comp+2);
-        next=random(2);
+        //next=random(2);
+        next = rand()%2;
         if(next==0) p2(1,&a[0]);
         else if(next==1) p3(1,&a[0]);
         return(&a[0]);
@@ -48,7 +34,8 @@ long *p2(int i,long *comp)
         b[i]++;
         if(*comp>b[0]) b[0]=*(comp);
         if(*(comp+2)>b[2]) b[2]=*(comp+2);
-        next=random(2);
+        //next=random(2);
+        next = rand()%2;
         if(next==0) p1(1,&b[0]);
         else if(next==1) p3(1,&b[0]);
         return &b[0];
@@ -64,9 +51,38 @@ long *p3(int i,long *comp)
         c[2]++;
         if(*comp>c[0]) c[0]=*(comp);
         if(*(comp+1)>c[1]) c[1]=*(comp+1);
-        next=random(2);
+        //next=random(2);
+        next = rand()%2;
         if(next==0) p1(1,&c[0]);
         return &c[0];
     }
     else return &c[0];
 }
+int main()
+{
+    long start[]= {0,0,0},*vector;
+    while(!kbhit())
+    {
+        p1(1,&start[0]);
+    }
+    printf("\n Process Vector\n");
+    vector=p1(0,&start[0]);
+    printf("p1[%ld/t%ld/t%ld]\n",*vector,*(vector+1),*(vector+2));
+    vector=p2(0,&start[0]);
+    printf("p2[%ld/t%ld/t%ld]\n",*vector,*(vector+1),*(vector+2));
+    vector=p3(0,&start[0]);
+    printf("p3[%ld/t%ld/t%ld]\n",*vector,*(vector+1),*(vector+2));
+}
+
+/*
+Process Vector
+p1[0/t0/t0]
+p2[0/t0/t0]
+p3[0/t0/t0]
+
+Process Vector
+p1[1/t0/t0]
+p2[0/t0/t0]
+p3[1/t0/t1]
+
+*/
